37e9fe4724e857b0c3a265c277ef17cda19821f3
eskaerpe <eskaerpe01@gmail.com>
2023-12-10 00:11:27 +0700

    add ma 5 orang

--- script.js
+++ script.js
@@ -1,31 +1,78 @@
-var hargaPaketA = 15000;
-var hargaPaketB = 20000;
-var hargaPaketC = 25000;
+// HARGA ALL ITEMS
+var hargaPaketA = 20000;
+var hargaPaketB = 25000;
+var hargaBouquet = 15000;
+var hargaBatangan = 10000;
 var hargaPesan = 5000;
 
 var jumlahPenerima = 1;
 
 var GFormsPengirimName = {
-    namaPengirim: "entry.287391154",
-    kelasPengirim: "entry.2133329921",
+    // Data Pengirim
     emailPengirim: "entry.1592840593",
     nomorTeleponPengirim: "entry.812587699",
+    namaPengirim: "entry.287391154",
+    kelasPengirim: "entry.2133329921",
 
+    // Data Penerima 1
     namaPenerima1: "entry.1336288915",
     kelasPenerima1: "entry.1967255455",
     paketPenerima1: "entry.851185307",
     pesanPenerima1: "entry.1519282563",
 
+    // Data Penerima 2
+    namaPengirim2: "entry.1352286003",
+    kelasPengirim2: "entry.508962244",
     namaPenerima2: "entry.1016256525",
     kelasPenerima2: "entry.821868320",
     paketPenerima2: "entry.1083087706",
     pesanPenerima2: "entry.551598349",
 
+    // Data Penerima 3
+    namaPengirim3: "entry.1978832972",
+    kelasPengirim3: "entry.1235934584",
     namaPenerima3: "entry.148643492",
     kelasPenerima3: "entry.1810191057",
     paketPenerima3: "entry.938679024",
     pesanPenerima3: "entry.1776113027",
+
+    // Data Penerima 4
+    namaPengirim4: "entry.747980189",
+    kelasPengirim4: "entry.376659577",
+    namaPenerima4: "entry.165216699",
+    kelasPenerima4: "entry.1379831424",
+    paketPenerima4: "entry.1701338243",
+    pesanPenerima4: "entry.659937213",
+
+    // Data Penerima 5
+    namaPengirim5: "entry.2135190722",
+    kelasPengirim5: "entry.1892512620",
+    namaPenerima5: "entry.515182497",
+    kelasPenerima5: "entry.866986164",
+    paketPenerima5: "entry.61340545",
+    pesanPenerima5: "entry.734914188",
 };
+
+function checkFieldsAndShowSection() {
+    // Dapatkan elemen input dan section
+    var namaPengirim = document.getElementById("nama-pengirim");
+    var kelasPengirim = document.getElementById("kelas-pengirim");
+    var emailPengirim = document.getElementById("email-pengirim");
+    var nomorTeleponPengirim = document.getElementById("nomor-telepon-pengirim");
+    var penerimaSection = document.getElementById("penerima-section");
+
+    // Periksa apakah semua field telah diisi
+    if (namaPengirim.value !== "" && kelasPengirim.value !== "" && emailPengirim.value !== "" && nomorTeleponPengirim.value !== "") {
+        // Jika semua field telah diisi, tampilkan section penerima
+        penerimaSection.style.display = "";
+        document.querySelector(".tombol-next").style.display = "none";
+        document.querySelector("#tombol-submit").style.display = "";
+        document.querySelector("#tambah-penerima-btn").style.display = "";
+    } else {
+        // Jika tidak, beri peringatan kepada pengguna
+        alert("Harap isi semua field data pengirim sebelum melanjutkan.");
+    }
+}
 
 // var elementPenerima = document.getElementById(IDPenerima);
 // var namaPenerimaValue = document.getElementById("nama-penerima").value;
@@ -44,7 +91,8 @@ function showHarga(IDPenerima) {
     // fetch all child  in element penerima
     var paketAchecked = elementPenerima.querySelector("#paket-A").checked;
     var paketBchecked = elementPenerima.querySelector("#paket-B").checked;
-    var paketCchecked = elementPenerima.querySelector("#paket-C").checked;
+    var bouquetChecked = elementPenerima.querySelector("#bouquet").checked;
+    var batanganChecked = elementPenerima.querySelector("#batangan").checked;
     var pesan = elementPenerima.querySelector("#pesan");
 
     var harga = elementPenerima.querySelector("#harga");
@@ -53,21 +101,21 @@ function showHarga(IDPenerima) {
 
     if (paketAchecked) {
         hargaSatuPenerima += hargaPaketA;
-        // hargaTotal += hargaPaketA;
     }
     if (paketBchecked) {
         hargaSatuPenerima += hargaPaketB;
-        // hargaTotal += hargaPaketB;
     }
-    if (paketCchecked) {
-        hargaSatuPenerima += hargaPaketC;
-        // hargaTotal += hargaPaketC;
+    if (bouquetChecked) {
+        hargaSatuPenerima += hargaBouquet;
+    }
+    if (batanganChecked) {
+        hargaSatuPenerima += hargaBatangan;
     }
 
-    if (paketAchecked || paketBchecked || paketCchecked) {
+    if (paketAchecked || paketBchecked || bouquetChecked || batanganChecked) {
         pesan.removeAttribute("disabled");
     }
-    if (!paketAchecked && !paketBchecked && !paketCchecked) {
+    if (!paketAchecked && !paketBchecked && !bouquetChecked && !batanganChecked) {
         pesan.setAttribute("disabled", "");
     }
     if (pesan.value != "") {
@@ -123,26 +171,45 @@ function addPenerima() {
         removeButton.setAttribute("id", `remove-button-${jumlahPenerima}`);
 
         // GOOGLE FORMS THINGY WITH THE NAME
+        var namaPengirimClone = newTr.querySelector("#nama-pengirim");
+        var kelasPengirimClone = newTr.querySelector("#kelas-pengirim");
+
         var namaPenerima = newTr.querySelector("#nama-penerima");
         var kelasPenerima = newTr.querySelector("#kelas-penerima");
         var paketPenerimaA = newTr.querySelector("#paket-A");
         var paketPenerimaB = newTr.querySelector("#paket-B");
-        var paketPenerimaC = newTr.querySelector("#paket-C");
+        var bouquetPenerima = newTr.querySelector("#bouquet");
+        var batanganPenerima = newTr.querySelector("#batangan");
         var pesanPenerima = newTr.querySelector("#pesan");
+
+        // Testing in console
+        console.log(namaPengirimClone);
+        console.log(kelasPengirimClone);
 
         console.log(namaPenerima);
         console.log(kelasPenerima);
         console.log(paketPenerimaA);
         console.log(paketPenerimaB);
-        console.log(paketPenerimaC);
+        console.log(bouquetPenerima);
+        console.log(batanganPenerima);
         console.log(pesanPenerima);
         // EDIT GOOGLE FORMS ATTRIBUTE NAME
+        namaPengirimClone.setAttribute("name", GFormsPengirimName[`namaPengirim${jumlahPenerima}`]); //
+        kelasPengirimClone.setAttribute("name", GFormsPengirimName[`kelasPengirim${jumlahPenerima}`]); //
+
         namaPenerima.setAttribute("name", GFormsPengirimName[`namaPenerima${jumlahPenerima}`]); //
         kelasPenerima.setAttribute("name", GFormsPengirimName[`kelasPenerima${jumlahPenerima}`]); //
         paketPenerimaA.setAttribute("name", GFormsPengirimName[`paketPenerima${jumlahPenerima}`]); //
         paketPenerimaB.setAttribute("name", GFormsPengirimName[`paketPenerima${jumlahPenerima}`]); //
-        paketPenerimaC.setAttribute("name", GFormsPengirimName[`paketPenerima${jumlahPenerima}`]); //
+        bouquetPenerima.setAttribute("name", GFormsPengirimName[`paketPenerima${jumlahPenerima}`]); //
+        batanganPenerima.setAttribute("name", GFormsPengirimName[`paketPenerima${jumlahPenerima}`]); //
         pesanPenerima.setAttribute("name", GFormsPengirimName[`pesanPenerima${jumlahPenerima}`]); //
+
+        namaPengirimClone.setAttribute("value", document.getElementById("nama-pengirim").value);
+        kelasPengirimClone.setAttribute("value", document.getElementById("kelas-pengirim").value);
+
+        console.log(namaPengirimClone.value);
+        console.log(kelasPengirimClone.value);
     } else if (jumlahPenerima >= 10) {
         alert("Maaf, jumlah penerima setiap transaksi maksimal sebanyak 10 orang. Silahkan selesaikan transaksi ini terlebih dahulu, lalu ulangi lagi dari awal dengan penerima yang berbeda.");
     }
